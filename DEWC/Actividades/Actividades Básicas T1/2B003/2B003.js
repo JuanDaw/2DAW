7615baf070cd2bbee7c6cb491fa12641a05b1fc6
Juan Antonio Villoria del Moral <juvi.13.13@gmail.com>
2020-10-27 18:53:33 +0100

    DWEC 27-10-20

--- DEWC/Actividades/Actividades Básicas T1/2B003/2B003.js
+++ DEWC/Actividades/Actividades Básicas T1/2B003/2B003.js
@@ -1,4 +1,25 @@
-var sala = document.getElementById("sal").value;
-var asesino = document.getElementById("ase").value;
-// console.log(sala);
-// console.log(asesino);
+function mifuncion() {
+    var sala = document.getElementById("sal").value;
+    var asesino = document.getElementById("ase").value;
+    console.log(sala);
+    console.log(asesino);
+    var items = {
+        baile: "veneno",
+        galeria: "trofeo",
+        billar: "palo de billar",
+        comedor: "cuchillo"
+    };
+    var disposicion = {
+        parkes: "comedor",
+        van: "galeria",
+        sparr: "billar",
+        kale: "baile"
+    }
+    console.log(disposicion);
+    if (sala == disposicion[asesino]) {
+        document.write("<p>" + asesino + " lo hizo en el " + disposicion[asesino] + " con el " + items[sala] + "!</p>");
+    } else {
+        document.write("<p>No has coincidido con el asesino, sigue intentando</p>");
+    }
+}
+    
